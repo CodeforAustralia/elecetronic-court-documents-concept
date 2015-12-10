291203b9a9b7d26ba0f5b608e548cd6fadd37f18
Ezekiel Kigbo <theatlasroom@gmail.com>
2015-12-11 09:20:31 +1100

    working on the send sms

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var compression = require('compression');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+var routes = require('./routes/admin');
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
